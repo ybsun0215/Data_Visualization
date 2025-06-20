134c44ced7a96bcf9001579888e427a007ea9996
xiaosun <48913347+ybsun0215@users.noreply.github.com>
2025-06-20 20:22:28 +0800

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,9 @@ const threatData = {
   threats: [
     { type: "Unauthorized accessing", counts: [57, 45, 19, 1, 6, 7, 0, 0] },
     { type: "Information leakage", counts: [53, 13, 27, 7, 1, 3, 0, 8] },
-    // ... 其他类型
+    { type: "Weak password", counts: [31, 14, 2, 1, 15, 6, 0, 0] },
+    { type: "DoS", counts: [1, 26, 7, 5, 5, 2, 1, 0] },
+    // 你可以根据需要继续添加其他类型
   ],
   riskLevels: { L: 245, M: 208, H: 66, C: 46 }
 };
